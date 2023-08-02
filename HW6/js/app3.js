3b6921d4662082b6810d9b1295494c9cc8b1d251
Eugene270987 <anikevich.it@gmail.com>
2023-08-02 12:42:46 +0300

    added HW6

--- HW6/js/app3.js
+++ HW6/js/app3.js
@@ -1,1 +1,24 @@
 // Дано ціле число. Вивести всі цілі числа від 1 до 100, квадрат яких не перевищує числа N
+
+let minValue = 1;
+let nNumber;
+
+do {
+    nNumber = parseInt(prompt('Enter your digit from ' + minValue));
+} while (minValue > nNumber);
+
+let minNumber = 1;
+let maxNumber = 100;
+
+let strNumber = "";
+
+for (let i = minNumber; i <= maxNumber; i++) {
+    let squareNumber = i**2;
+    if (squareNumber <= nNumber) {
+        strNumber += `${i} `;
+    }
+}
+
+alert(strNumber);
+
+// DONE
