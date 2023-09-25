29a9519f2835e42ddb60b73dec742d50ccb8b35b
Eugene270987 <anikevich.it@gmail.com>
2023-09-25 14:14:29 +0300

    Added changes

--- HW20/js/main.js
+++ HW20/js/main.js
@@ -1,10 +1,8 @@
-'use strict'
+'use strict';
 
 const users = JSON.parse(localStorage.getItem('users')) || defaultUsers;
 
 showInfo(users);
-
-
 
 document.querySelector('#btnAdd input').addEventListener('click',  showAddUserForm);
 
