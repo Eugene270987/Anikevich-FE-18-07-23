6b46d652f5ae393eaa2d52ab728d0ddb474cf8eb
Eugene270987 <anikevich.it@gmail.com>
2023-11-20 13:27:24 +0200

    made changes to form validation using formik and yup

--- hw-react-2/src/components/ContactForm/ContactForm.jsx
+++ hw-react-2/src/components/ContactForm/ContactForm.jsx
@@ -1,33 +1,97 @@
 import './ContactForm.scss';
-import {useRef, useState} from 'react';
+import {Fragment} from 'react';
 import {useNavigate} from "react-router-dom";
+import {useFormik} from 'formik';
+import {string, object} from 'yup';
+
+const formElementsConfig = {
+    name: {
+        defaultValue: '',
+        inputType: 'text',
+        placeholder: 'Enter your name',
+        labelText: 'Name',
+        vSchema: string()
+            .min(5, 'Your Name is too short!')
+            .max(15, 'Your Name is too long')
+            .required('Name required!'),
+    },
+    username: {
+        defaultValue: '',
+        inputType: 'text',
+        placeholder: 'Enter your username',
+        labelText: 'Username',
+        vSchema: string()
+            .min(5, 'Your Username is too short!')
+            .max(15, 'Your Username is too long')
+            .required('Username required!'),
+    },
+    phone: {
+        defaultValue: '',
+        inputType: 'text',
+        placeholder: '0671234567',
+        labelText: 'Phone Number',
+        vSchema: string()
+            .length(10, 'Please, enter correct phone number!')
+            .required('Phone number required!'),
+    },
+}
+
+function getInitialValues () {
+    let initialValues = {};
+    for (let key of Object.keys(formElementsConfig)) {
+        initialValues[key] = formElementsConfig[key].defaultValue;
+    }
+    return initialValues;
+}
+
+function getValidationSchema () {
+    let objWithSchemas = {};
+    for (let key of Object.keys(formElementsConfig)) {
+        objWithSchemas[key] = formElementsConfig[key].vSchema;
+    }
+    return object(objWithSchemas);
+}
+
 function ContactForm ({onAddContact}) {
-    const nameRef = useRef('');
-    const userNameRef = useRef('');
-    const phoneNumberRef = useRef('');
-    const [error, setError] = useState('');
     const navigate = useNavigate();
 
-    const handleSaveContact = () => {
-        const name = nameRef.current.value;
-        const username = userNameRef.current.value;
-        const phone = phoneNumberRef.current.value;
+    const formik = useFormik({
+        initialValues: getInitialValues(),
+        validationSchema: getValidationSchema(),
+        onSubmit: values => {
+            onAddContact (values);
+            navigate('/ContactList');
+        }
+    })
 
-        if (!name || !username || !phone) {
-            setError('Please fill all the fields below!!!');
-            return;
+    const generateFormElements = () => {
+        const formItems = [];
+        for (let key of Object.keys(formElementsConfig)) {
+            const { labelText, inputType, placeholder } = formElementsConfig[key];
+
+            formItems.push((
+                <Fragment key={key}>
+                    <div className="input__wrapper">
+                        <label className="input__label" htmlFor={key}>{labelText}</label>
+                        <input type={inputType} id={key} name={key} value={formik.values[key]} onChange={formik.handleChange} placeholder={placeholder}/>
+                        {formik.errors[key] && <p className="error">{formik.errors[key]}</p>}
+                    </div>
+                </Fragment>
+            ))
         }
 
-        setError('');
-
-        onAddContact({
-            name,
-            username,
-            phone,
-        });
-
-        navigate('/ContactList');
+        return (
+            <fieldset className="form__inner">
+                <legend className="form__title">Fill in the following form</legend>
+                {formItems}
+                <div className="button__wrapper">
+                    <button className="btn-cancel" type="button" onClick={handleCancel}>Cancel</button>
+                    <button className="btn-save" type="button" onClick={formik.handleSubmit}>Save</button>
+                </div>
+            </fieldset>
+        )
     }
+
 
     const handleCancel = () => {
         navigate('/ContactList');
@@ -35,26 +99,7 @@ function ContactForm ({onAddContact}) {
 
     return (
         <form className="form">
-            <fieldset className="form__inner">
-                <legend className="form__title">Fill in the following form</legend>
-                {error && <p className="error__message">{error}</p>}
-                <div className="input__wrapper">
-                    <label className="input__label" htmlFor="name">Name:</label>
-                    <input type="text" ref={nameRef} id="name"/>
-                </div>
-                <div className="input__wrapper">
-                    <label className="input__label" htmlFor="username">Username:</label>
-                    <input type="text" ref={userNameRef} id="username"/>
-                </div>
-                <div className="input__wrapper">
-                    <label className="input__label" htmlFor="number">Phone Number:</label>
-                    <input type="text" ref={phoneNumberRef} id="number"/>
-                </div>
-                <div className="button__wrapper">
-                    <button className="btn-cancel" type="button" onClick={handleCancel}>Cancel</button>
-                    <button className="btn-save" type="button" onClick={handleSaveContact}>Save</button>
-                </div>
-            </fieldset>
+            {generateFormElements()}
         </form>
     )
 }
